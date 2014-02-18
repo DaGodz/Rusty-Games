ed432a4b6df5ab2dc7017b83764b874b2d5f8442
pljhll <paul.hill@doovoo.net>
2014-02-18 22:08:06 +0000

    Players only get one life. Once they die, they are out.
    Added config file and default config.
    Added reset config command.
    Added basic logging.
    Added kick user after they have died.
    Bumped version to 0.2

--- rustygames.lua
+++ rustygames.lua
@@ -1,10 +1,63 @@
 PLUGIN.Title = "Rusty Games"
 PLUGIN.Description = "An Oxide plugin inspired by movies such as The Hunger Games and Battle Royale."
+PLUGIN.Author = "DaGodz"
+PLUGIN.Version = "0.2"
+
+deadUsers = {}
 
 function PLUGIN:Init()
-  self:AddChatCommand("tinroof", self.cmdTinRoof)
+  print("Loading " .. self.Title .. " (" .. self.Version .. ")...")
+  self:LoadConfig(false)
+  self:AddChatCommand("tinroof", self.CmdTinRoof)
+  self:AddCommand("rustygames", "resetconfig", self.CmdResetConfig)
 end
 
-function PLUGIN:cmdTinRoof(netuser, cmd, args)
-  rust.Notice(netuser,"Rusty!",10000)
+function PLUGIN:LoadConfig(isReset)
+	local b, res = config.Read("rustygames")
+	self.Config = res or {}
+	if (not b or isReset) then
+		self:LoadDefaultConfig()
+		if (res) then config.Save("rustygames") end
+	end
+end
+
+function PLUGIN:LoadDefaultConfig()
+  self.Config.chatName = "RustyGames"
+  self.Config.broadcastChat = true
+  self.Config.log = true
+end
+
+function PLUGIN:CmdResetConfig(netUser, cmd, args)
+  self:LoadConfig(true)
+  rust.Notice(netUser,"Config reset")
+end
+
+function PLUGIN:BroadcastChat(msg)
+  if (self.Config.broadcastChat) then rust.BroadcastChat(self.Config.chatName, msg) end
+end
+
+function PLUGIN:Log(msg)
+  if (self.Config.log) then print(self.Title .. ": " .. msg) end
+end
+
+function PLUGIN:CmdTinRoof(netUser, cmd, args)
+  rust.Notice(netUser,"Rusty!")
+end
+
+function PLUGIN:OnKilled(takeDamage, damage)
+  if (takeDamage:GetComponent("HumanController" )) then
+    userName = damage.victim.client.userName
+    deadUsers[userName] = true
+    self:Log(userName .. " died.")
+  end
+end
+
+function PLUGIN:OnSpawnPlayer(playerClient, useCamp, avatar)
+  local userName = playerClient.userName
+  if (deadUsers[userName]) then
+    self:BroadcastChat(userName .. " is out of this Rusty Games!")
+    playerClient.netUser:Kick(NetError.Facepunch_Kick_RCON, true)
+    self:Log(userName .. " kicked for dying.")
+    deadUsers[userName] = nil
+  end
 end

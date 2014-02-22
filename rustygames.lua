7021135652bc9126d0feef6ac3be9c666fe4be3f
pljhll <paul.hill@doovoo.net>
2014-02-22 00:17:28 +0000

    Games can be started/stopped any time by an admin
    Rusty Games rules are now only applied when games are in progress
    Added /startthegames and /stopthegames chat commands
    Default chat name changed to "The Rusty Announcer"
    Chat announcements moved to config file and given defaults
    Corrected the Love Shack
    Bumped version to 0.3

--- rustygames.lua
+++ rustygames.lua
@@ -1,15 +1,23 @@
 PLUGIN.Title = "Rusty Games"
 PLUGIN.Description = "An Oxide plugin inspired by movies such as The Hunger Games and Battle Royale."
 PLUGIN.Author = "DaGodz"
-PLUGIN.Version = "0.2"
+PLUGIN.Version = "0.3"
 
-deadUsers = {}
+if (not rustyGames) then
+  rustyGames = {}
+  rustyGames.deadUsers = {}
+  rustyGames.inProgress = false
+end
 
 function PLUGIN:Init()
   print("Loading " .. self.Title .. " (" .. self.Version .. ")...")
   self:LoadConfig(false)
-  self:AddChatCommand("tinroof", self.CmdTinRoof)
-  self:AddCommand("rustygames", "resetconfig", self.CmdResetConfig)
+
+  self:AddChatCommand("startthegames", self.CmdStartTheGames)
+  self:AddChatCommand("stopthegames", self.CmdStopTheGames)
+  self:AddChatCommand("tinroof...", self.CmdTinRoof) -- a bit of fun
+
+  self:AddCommand("rustygames", "resetconfig", self.CCmdResetConfig)
 end
 
 function PLUGIN:LoadConfig(isReset)
@@ -22,14 +30,68 @@ function PLUGIN:LoadConfig(isReset)
 end
 
 function PLUGIN:LoadDefaultConfig()
-  self.Config.chatName = "RustyGames"
+  self.Config.chatName = "The Rusty Announcer"
   self.Config.broadcastChat = true
   self.Config.log = true
+  
+  self.Config.say = {}
+  self.Config.say.start = "Let the Rusty Games Commence!"
+  self.Config.say.stop = "The Rusty Games have been abandoned!"
+  self.Config.say.died = "is out of this Games!"
 end
 
-function PLUGIN:CmdResetConfig(netUser, cmd, args)
+function PLUGIN:CCmdResetConfig(arg)
   self:LoadConfig(true)
-  rust.Notice(netUser,"Config reset")
+  arg:ReplyWith("Rusty Games config reset")
+  self:Log("Config reset")
+end
+
+function PLUGIN:CmdTinRoof(netUser, cmd, args)
+  rust.Notice(netUser,"Rusted!")
+end
+
+function PLUGIN:CmdStartTheGames(netUser, cmd, args)
+  if (netUser and not netUser:canAdmin()) then return end
+  
+  if (rustyGames.inProgress) then
+    rust.Notice(netUser,"The Rusty Games are already in progress! You can abandon them with /stopthegames")
+  else
+    rustyGames.deadUsers = {}
+    rustyGames.inProgress = true
+    self:BroadcastChat(self.Config.say.start)
+  end
+end
+
+function PLUGIN:CmdStopTheGames(netUser, cmd, args)
+  if (netUser and not netUser:canAdmin()) then return end
+  
+  if (not rustyGames.inProgress) then
+    rust.Notice(netUser,"The Rusty Games are not in progress! You can start them with /startthegames")
+  else
+    rustyGames.deadUsers = {}
+    rustyGames.inProgress = false
+    self:BroadcastChat(self.Config.say.stop)
+  end
+end
+
+function PLUGIN:OnKilled(takeDamage, damage)
+  if (not rustyGames.inProgress) then return end
+  if (takeDamage:GetComponent("HumanController" )) then
+    userName = damage.victim.client.userName
+    rustyGames.deadUsers[userName] = true
+    self:Log(userName .. " died.")
+  end
+end
+
+function PLUGIN:OnSpawnPlayer(playerClient, useCamp, avatar)
+  if (not rustyGames.inProgress) then return end
+  local userName = playerClient.userName
+  if (rustyGames.deadUsers[userName]) then
+    self:BroadcastChat(userName .. " " .. self.Config.say.died)
+    playerClient.netUser:Kick(NetError.Facepunch_Kick_RCON, true)
+    self:Log(userName .. " kicked for dying.")
+    rustyGames.deadUsers[userName] = nil
+  end
 end
 
 function PLUGIN:BroadcastChat(msg)
@@ -38,26 +100,4 @@ end
 
 function PLUGIN:Log(msg)
   if (self.Config.log) then print(self.Title .. ": " .. msg) end
-end
-
-function PLUGIN:CmdTinRoof(netUser, cmd, args)
-  rust.Notice(netUser,"Rusty!")
-end
-
-function PLUGIN:OnKilled(takeDamage, damage)
-  if (takeDamage:GetComponent("HumanController" )) then
-    userName = damage.victim.client.userName
-    deadUsers[userName] = true
-    self:Log(userName .. " died.")
-  end
-end
-
-function PLUGIN:OnSpawnPlayer(playerClient, useCamp, avatar)
-  local userName = playerClient.userName
-  if (deadUsers[userName]) then
-    self:BroadcastChat(userName .. " is out of this Rusty Games!")
-    playerClient.netUser:Kick(NetError.Facepunch_Kick_RCON, true)
-    self:Log(userName .. " kicked for dying.")
-    deadUsers[userName] = nil
-  end
 end
